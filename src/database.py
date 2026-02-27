65dadbe64d2900e25d37957f6054fcb928ce199b
farzadoxo <farzadebrahimi.oxo@gmail.com>
2026-02-27 20:09:21 +0330

    room mechanisem added
    create room - get rooms - enter a room

--- src/database.py
+++ src/database.py
@@ -22,7 +22,7 @@ import json
 
 
 redis_client = Redis(
-    host='redis',
+    host='127.0.0.1',
     port=6379,
     decode_responses=True
 )
--- src/models.py
+++ src/models.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class Room(BaseModel):
     name : str
     user_limit : int
-
+    visable : bool
     
 class Message(BaseModel):
     content : str
