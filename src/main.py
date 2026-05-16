d8c688d296c616c948d6cc5b7393839956c88657
farzadoxo <farzadebrahimi.oxo@gmail.com>
2026-05-16 13:34:36 +0330

    - project runner updated

--- src/main.py
+++ src/main.py
@@ -4,15 +4,17 @@ import dotenv
 import os
 
 
-dotenv.load_dotenv()    
-IP = os.getenv("IP")
+dotenv.load_dotenv('')    
+HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
 
+print(HOST)
+print(PORT)
 
 
 if __name__ == "__main__":
     import core.database
-    uvicorn.run(app,host=IP,port=int(PORT))
+    uvicorn.run(app,host=HOST,port=int(PORT))
 
 # you can run project without this sentence! run:
 #       python3 -m uvicorn main:app --host 'ip' --port 'port'
